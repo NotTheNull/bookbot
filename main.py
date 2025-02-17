272f3b6b78a74b982ef2e8faacc57e01478956f7
NotTheNull <objnotnull@live.com>
2025-02-16 22:15:12 -0700

    modifying main.py to print a dictionary of character counts

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 def main(path_to_file):
     text_file = read_file(path_to_file)
-    word_count = count_words(text_file)
-    print(word_count)
+    #word_count = count_words(text_file)
+    #print(word_count)
+
+    result = count_characters(text_file)
+    print(result)
 
 def read_file(path_to_file):
     file_contents = ""
@@ -13,7 +16,18 @@ def read_file(path_to_file):
     return file_contents
 
 def count_words(text):
-    #words = text.split()
     return len(text.split())
+
+def count_characters(text):
+    #ignore case
+    char_count = {}
+    for c in text.lower():
+        count = 0
+        if c in char_count:
+            count = char_count[c]
+        count = count + 1
+        char_count[c] = count
+
+    return char_count
 
 main("./books/frankenstein.txt")

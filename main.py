30e1e792ea282b64ee7dc293fccd2e7ae6b6b186
NotTheNull <objnotnull@live.com>
2025-02-16 21:56:36 -0700

    Modified main to perform a word count of the book

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # Read contents of books
 
 def main(path_to_file):
+    text_file = read_file(path_to_file)
+    word_count = count_words(text_file)
+    print(word_count)
+
+def read_file(path_to_file):
+    file_contents = ""
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        
+    return file_contents
+
+def count_words(text):
+    #words = text.split()
+    return len(text.split())
 
 main("./books/frankenstein.txt")

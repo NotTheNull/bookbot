2a93a0384698553fcfcd3bb6ba3b258683bd1f4c
NotTheNull <objnotnull@live.com>
2025-02-17 19:37:39 -0700

    finished ability to build report on book

--- main.py
+++ main.py
@@ -2,11 +2,39 @@
 
 def main(path_to_file):
     text_file = read_file(path_to_file)
-    #word_count = count_words(text_file)
-    #print(word_count)
-
+    word_count = count_words(text_file)
     result = count_characters(text_file)
-    print(result)
+    list_counts = convert_and_sort(result)
+    report = build_report(path_to_file, word_count, list_counts)
+    print(report)
+
+
+def build_report(path_to_file, word_count, char_counts):
+    report = f"--- Begin report of {path_to_file} ---\n"
+    report += f"{word_count} words found in the document\n"
+
+    #NOTE: we only want the alphabet characters
+    for c in char_counts:
+        if c["char"] < 'a': continue
+        if c["char"] > 'z': continue
+        report += f"\nThe '{c["char"]}' character was found {c["count"]} times"
+
+    report += "\n--- End report ---"
+    return report
+
+def convert_and_sort(dict_chars):
+    list_chars = []
+    
+    for c in dict_chars:
+        json = {"char": c, "count": dict_chars[c]}
+        list_chars.append(json)
+
+    list_chars.sort(reverse=True, key=sort_char_count)
+    return list_chars
+
+def sort_char_count(dict):
+    return dict["count"]
+
 
 def read_file(path_to_file):
     file_contents = ""
